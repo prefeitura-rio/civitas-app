4e446af2144006d2058b5d9a17e9a7e8b6f6ee75
Lucas Tavares <lucastavarestt@gmail.com>
2024-06-04 15:20:50 -0300

    style: appbar button style

--- src/components/AppBarNav/AppBarNav.js
+++ src/components/AppBarNav/AppBarNav.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { AppBar, Toolbar, Button, Typography, Divider } from '@mui/material';
+import { AppBar, Toolbar, Button, Typography, Divider, Box } from '@mui/material';
 import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
 import { CAMERAS_UNDER_APP_BAR, HOME_UNDER_APP_BAR, MAPAS_UNDER_APP_BAR, PROTOCOLOS_UNDER_APP_BAR, CERCO_DIGITAL_UNDER_APP_BAR, LOGIN_UNDER_APP_BAR } from '../../redux/active/actions';
 import civitas_icon from '../../assets/civitas_icon.png';
-const AppBarNav = ({ setActiveUnderAppBar }) => {
+const AppBarNav = ({ setActiveUnderAppBar, activeUnderAppBar }) => {
   return (
     <AppBar style={{ backgroundColor: 'black' }}>  {/* <AppBar position="static"> */}
       <Toolbar>
@@ -11,12 +11,26 @@ const AppBarNav = ({ setActiveUnderAppBar }) => {
         <Divider orientation="vertical" style={{ marginLeft: 40, marginRight: 40, height: '33px', width: '6px', backgroundColor: '#23C1F1' }} />
         <Typography variant="h6" style={{ color: 'white' }}>DISQUE DENÚNCIA</Typography>
         <div style={{ marginLeft: 'auto' }}>
-          {/* <Button color="inherit" onClick={() => setActiveUnderAppBar(HOME_UNDER_APP_BAR)}>Home</Button>
-            <Button color="inherit" onClick={() => setActiveUnderAppBar(CAMERAS_UNDER_APP_BAR)}>Câmeras</Button>
-            <Button color="inherit" onClick={() => setActiveUnderAppBar(MAPAS_UNDER_APP_BAR)}>Mapas</Button>
-            <Button color="inherit" onClick={() => setActiveUnderAppBar(PROTOCOLOS_UNDER_APP_BAR)}>Protocolos</Button> */}
-          <Button color="inherit" onClick={() => setActiveUnderAppBar(CERCO_DIGITAL_UNDER_APP_BAR)}>Cerco Digital</Button>
-          <Button color="inherit" onClick={() => setActiveUnderAppBar(LOGIN_UNDER_APP_BAR)}>Login</Button>
+          <Button
+            style={activeUnderAppBar === CERCO_DIGITAL_UNDER_APP_BAR ? { backgroundColor: 'grey', borderRadius: '20px' } : {}}
+            color="inherit"
+            onClick={() => setActiveUnderAppBar(CERCO_DIGITAL_UNDER_APP_BAR)}
+            sx={{ paddingLeft:"15px",paddingRight:"15px"}}
+          >
+            {activeUnderAppBar === CERCO_DIGITAL_UNDER_APP_BAR &&
+              <Box sx={{ width: 10, height: 10, borderRadius: '50%', backgroundColor: '#23C1F1', marginRight: 1 }} />}
+            <Typography>Cerco Digital</Typography>
+          </Button>
+          <Button
+            style={activeUnderAppBar === LOGIN_UNDER_APP_BAR ? { backgroundColor: 'grey', borderRadius: '20px' } : {}}
+            color="inherit"
+            onClick={() => setActiveUnderAppBar(LOGIN_UNDER_APP_BAR)}
+            sx={{ paddingLeft:"15px",paddingRight:"15px"}}
+          >
+            {activeUnderAppBar === LOGIN_UNDER_APP_BAR &&
+              <Box sx={{ width: 10, height: 10, borderRadius: '50%', backgroundColor: '#23C1F1', marginRight: 1 }} />}
+            <Typography>Login</Typography>
+          </Button>
         </div>
       </Toolbar>
     </AppBar>
