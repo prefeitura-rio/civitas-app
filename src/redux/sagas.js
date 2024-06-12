a411f4702dbc9671a7637360c444ecc6dd18f5d5
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2024-06-12 15:09:47 -0300

    Revert "New feats"

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,11 +1,10 @@
 import { all, call, fork, put, takeEvery } from "redux-saga/effects";
 import { LOAD_ACTIVE_CALLS, requestActiveCalls, requestActiveCallsFailed, requestActiveCallsSuccess } from "./calls/actions";
-import { api, getActiveCalls, getCarsPath, login } from "../api";
-import { LOGIN, LOG_OUT, logOut, loginFail, loginRequest, loginSuccess } from "./auth/actions";
+import { getActiveCalls, getCarsPath, login } from "../api";
+import { LOGIN, LOG_OUT, loginFail, loginRequest, loginSuccess } from "./auth/actions";
 import { LOAD_CARS_PATH, requestCarsPath, requestCarsPathFailed, requestCarsPathSuccess } from "./cars/actions";
-import SnackBar from "../utils/SnackBar";
+import Error from "../utils/Error";
 import { showError } from "./error/actions";
-import { LOGIN_UNDER_APP_BAR, setActiveUnderAppBar } from "./active/actions";
 
 
 // -----------------------------------------------------//
@@ -44,9 +43,7 @@ function* workerLoadCarsPath(action) {
             yield put(showError({ message: "Solicitação inválida. Por favor, verifique os parâmetros da sua solicitação.", status: 400 }));
             break;
           case 401:
-            yield put(showError({ message: "Sua sessão expirou. Por favor, entre novamente.", status: 401, severity: "info" }));
-            yield put(logOut());
-            yield put(setActiveUnderAppBar(LOGIN_UNDER_APP_BAR));
+            yield put(showError({ message: "Falha na autenticação. Por favor, verifique suas credenciais e tente novamente.", status: 401 }));
             break;
           case 403:
             yield put(showError({ message: "Acesso proibido. Você não tem permissão para acessar este recurso.", status: 403 }));
@@ -86,16 +83,13 @@ function* workerLogin(action) {
       if (error.response) {
         switch (error.response.status) {
           case 401:
-            yield put(showError({ message: "Falha na autenticação. Por favor, verifique suas credenciais e tente novamente.", status: error.response.status, severity: "warning" }));
+            yield put(showError({ message: "Falha na autenticação. Por favor, verifique suas credenciais e tente novamente.", status: 401 }));
             break;
           case 403:
-            yield put(showError({ message: "Acesso proibido. Você não tem permissão para acessar este recurso.", status: error.response.status }));
+            yield put(showError({ message: "Acesso proibido. Você não tem permissão para acessar este recurso.", status: 403 }));
             break;
           case 500:
-            yield put(showError({ message: "Erro interno do servidor. Por favor, tente novamente mais tarde.", status: error.response.status }));
-            break;
-          case 400:
-            yield put(showError({ message: "Login ou senha inválido(s). Por favor, tente novamente!", status: error.response.status, severity: "warning" }));
+            yield put(showError({ message: "Erro interno do servidor. Por favor, tente novamente mais tarde.", status: 500 }));
             break;
           default:
             yield put(showError({ message: "Um erro desconhecido ocorreu. Por favor, tente novamente mais tarde.", status: error.response.status }));
@@ -117,8 +111,8 @@ export function* watchLogin() {
 
 export function* rootSaga() {
   yield all([
-    fork(watchLoadActiveCalls),
-    fork(watchLoadCarsPath),
-    fork(watchLogin),
+   fork(watchLoadActiveCalls),
+   fork(watchLoadCarsPath),
+   fork(watchLogin),
   ]);
 }
