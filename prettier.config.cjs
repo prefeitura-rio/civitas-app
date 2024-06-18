d94a2f4bf42267bbffcbbb6357d8acf171478df3
Victor Assis <victor_assis@poli.ufrj.br>
2024-06-18 10:16:09 -0300

    feat(Config): :wrench: Configura estrutura do projeto
    Cria projeto next.js, configura ESlint, Prettier, Tailwind, TSconfig, etc.

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  bracketSameLine: false,
+}
