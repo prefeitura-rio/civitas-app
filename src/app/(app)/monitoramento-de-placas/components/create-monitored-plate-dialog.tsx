ad618ca00cbc87d58cac061e3cbdd6b5c83a1f73
Victor Assis <victor_assis@poli.ufrj.br>
2024-06-26 08:03:19 -0300

    feat(MonitoramentoDePlacas): :sparkles: Implementa fieldArray para canais de notificação

--- src/app/(app)/monitoramento-de-placas/components/create-monitored-plate-dialog.tsx
+++ src/app/(app)/monitoramento-de-placas/components/create-monitored-plate-dialog.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { Plus, Trash } from 'lucide-react'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -17,6 +18,11 @@ import { Label } from '@/components/ui/label'
 
 const newPlateFormSchema = z.object({
   plate: z.string().min(1, { message: 'Campo obrigatório' }),
+  notificationChannels: z.array(
+    z.object({
+      channel: z.string(),
+    }),
+  ),
 })
 
 type NewPlateForm = z.infer<typeof newPlateFormSchema>
@@ -25,14 +31,27 @@ export function CreateMonitoredPlateDialog() {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm<NewPlateForm>({
     resolver: zodResolver(newPlateFormSchema),
+    defaultValues: {
+      notificationChannels: [{ channel: '' }],
+    },
+  })
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'notificationChannels',
   })
 
   function onSubmit(props: NewPlateForm) {
-    // ...
-    console.log(props)
+    const notificationChannels = props.notificationChannels.map(
+      (item) => item.channel,
+    )
+    console.log({
+      plate: props.plate,
+      notificationChannels,
+    })
   }
 
   return (
@@ -52,6 +71,35 @@ export function CreateMonitoredPlateDialog() {
             <InputError message={errors.plate?.message} />
           </div>
           <Input id="plate" type="text" {...register('plate')} />
+          <div className="mt-4 flex flex-col gap-4">
+            <div className="flex items-center gap-2">
+              <Label>Canais de notificação</Label>
+              <Button
+                variant="secondary"
+                type="button"
+                className="h-8 w-8 p-0"
+                onClick={() => append({ channel: '' })}
+              >
+                <Plus className="h-4 w-4" />
+              </Button>
+            </div>
+            <div className="flex flex-col gap-2">
+              {fields.map((field, index) => (
+                <div className="flex items-center gap-2">
+                  <Input
+                    {...register(`notificationChannels.${index}.channel`)}
+                  />
+                  <Button
+                    variant="destructive"
+                    className="h-8 w-8 p-0"
+                    onClick={() => remove(index)}
+                  >
+                    <Trash className="h-4 w-4" />
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
         <DialogFooter className="flex justify-end">
           <Button type="submit">Adicionar</Button>
