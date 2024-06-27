e72d38fdd62d97fa829cb7b7f7f4ce786b6b633d
Victor Assis <victor_assis@poli.ufrj.br>
2024-06-27 16:53:24 -0300

    feat(MonitoramentoDePlacas): :sparkles: Implementa disable de ações para usuários não admins

--- src/app/(app)/monitoramento-de-placas/components/columns.tsx
+++ src/app/(app)/monitoramento-de-placas/components/columns.tsx
@@ -21,51 +21,61 @@ const UpdateMonitoredPlateDialog = dynamic(
   },
 )
 
-export const columns: ColumnDef<MonitoredPlate>[] = [
-  {
-    accessorKey: 'plate',
-    header: 'Placa',
-  },
-  {
-    accessorKey: 'additional_info',
-    header: 'Informações adicionais',
-    cell: ({ row }) => {
-      return <div>{JSON.stringify(row.getValue('additional_info') || '')}</div>
+interface GetColumnsProps {
+  isAdmin: boolean
+}
+export function getColumns({ isAdmin }: GetColumnsProps) {
+  console.log(isAdmin)
+  const columns: ColumnDef<MonitoredPlate>[] = [
+    {
+      accessorKey: 'plate',
+      header: 'Placa',
     },
-  },
-  {
-    id: 'edit',
-    cell: ({ row }) => (
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Editar linha</span>
-            <PencilLine className="h-4 w-4" />
-          </Button>
-        </DialogTrigger>
-        <UpdateMonitoredPlateDialog
-          plate={row.original.plate}
-          additionalInfo={row.original.additional_info}
-          notificationChannels={row.original.notification_channels}
-        />
-      </Dialog>
-    ),
-  },
-  {
-    id: 'delete',
-    cell: ({ row }) => (
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Excluir linha</span>
-            <Trash className="h-4 w-4" />
-          </Button>
-        </AlertDialogTrigger>
-        <DeleteMonitoredPlateAlertDialog
-          id={row.original.id}
-          plate={row.original.plate}
-        />
-      </AlertDialog>
-    ),
-  },
-]
+    {
+      accessorKey: 'additional_info',
+      header: 'Informações adicionais',
+      cell: ({ row }) => {
+        return (
+          <div>{JSON.stringify(row.getValue('additional_info') || '')}</div>
+        )
+      },
+    },
+    {
+      id: 'edit',
+      cell: ({ row }) => (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0" disabled={!isAdmin}>
+              <span className="sr-only">Editar linha</span>
+              <PencilLine className="h-4 w-4" />
+            </Button>
+          </DialogTrigger>
+          <UpdateMonitoredPlateDialog
+            plate={row.original.plate}
+            additionalInfo={row.original.additional_info}
+            notificationChannels={row.original.notification_channels}
+          />
+        </Dialog>
+      ),
+    },
+    {
+      id: 'delete',
+      cell: ({ row }) => (
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0" disabled={!isAdmin}>
+              <span className="sr-only">Excluir linha</span>
+              <Trash className="h-4 w-4" />
+            </Button>
+          </AlertDialogTrigger>
+          <DeleteMonitoredPlateAlertDialog
+            id={row.original.id}
+            plate={row.original.plate}
+          />
+        </AlertDialog>
+      ),
+    },
+  ]
+
+  return columns
+}
