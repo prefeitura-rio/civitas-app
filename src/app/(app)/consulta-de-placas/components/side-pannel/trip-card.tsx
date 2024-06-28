657c7fbc611a32053ccd8b9c07e1622bccd597a8
Victor Assis <victor_assis@poli.ufrj.br>
2024-06-28 13:55:10 -0300

    feat(ConsultaDePlacas): :recycle: Refatora componentes da listagem de viagens e pontos; Corrige pequeno bug de layout

--- src/app/(app)/consulta-de-placas/components/side-pannel/trip-card.tsx
+++ src/app/(app)/consulta-de-placas/components/side-pannel/trip-card.tsx
@@ -2,10 +2,11 @@ import { format } from 'date-fns'
 import { ChevronRight } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
 
-import { Tooltip } from '@/components/ui/tooltip'
 import { useCarPath } from '@/hooks/useCarPathContext'
 import type { Point } from '@/utils/formatCarPathResponse'
 import { toPascalCase } from '@/utils/toPascalCase'
+
+import { PointCard } from './point-card'
 
 interface TripCardProps {
   index: number
@@ -35,17 +36,11 @@ export function TripCard({ index, startLocation, endLocation }: TripCardProps) {
     trips,
     selectedTripIndex,
   } = useCarPath()
-  // const [shouldExpand, setShouldExpand] = useState(true)
 
   const points = trips?.at(index)?.points || []
   const isSelected = selectedTripIndex === index
 
-  // if (!isSelected && shouldExpand) {
-  // setShouldExpand(false)
-  // }
-
   function handleTripClick() {
-    // setShouldExpand(!shouldExpand)
     setSelectedTripIndex(index)
     const longitude = points?.at(0)?.from[0]
     const latitude = points?.at(0)?.from[1]
@@ -56,21 +51,9 @@ export function TripCard({ index, startLocation, endLocation }: TripCardProps) {
     })
   }
 
-  function handlePointClick(pointIndex: number) {
-    console.log(pointIndex)
-    const longitude = points?.at(pointIndex)?.from[0]
-    const latitude = points?.at(pointIndex)?.from[1]
-    setViewport({
-      ...viewport,
-      longitude: longitude || viewport.longitude,
-      latitude: latitude || viewport.latitude,
-    })
-  }
-
   const startTime = new Date(startLocation.startTime)
   const endTime = new Date(endLocation.startTime)
   const dayInterval = endTime.getDate() - startTime.getDate()
-  // const dayInterval = 1
   const timeInterval = getTimeDiff(startTime, endTime)
   const totalIndexes = endLocation.index - startLocation.index
 
@@ -124,55 +107,20 @@ export function TripCard({ index, startLocation, endLocation }: TripCardProps) {
         )}
       </div>
 
-      {/* Third row: Details */}
-      {/* <div className="pl-20">
-        <Button
-          className="h-4 p-0"
-          variant="link"
-          onClick={(e) => {
-            e.stopPropagation()
-            setOpenDetails(!openDetails)
-          }}
-        >
-          Trajeto
-        </Button>
-      </div> */}
-
-      {/* {shouldExpand && ( */}
+      {/* Point List */}
       {isSelected && (
         <div className="flex flex-col pl-20 text-sm">
           {points.length > 0 ? (
             points.map((point, index) => (
-              <div
-                className="relative flex gap-2 rounded-md hover:bg-card"
-                onClick={(e) => {
-                  e.stopPropagation()
-                  handlePointClick(index)
-                }}
-              >
-                <span className="w-16 shrink-0">
-                  {format(new Date(point.startTime), 'hh:mm aa')}
-                </span>
-                {index < points.length - 1 && (
-                  <div className="absolute left-[4.8rem] top-0 mt-2 h-full w-0.5 bg-primary" />
-                )}
-                <div className="z-10 mt-1.5 h-3 w-3 shrink-0 rounded-full border-2 border-primary bg-card" />
-                <div className="ml-1.5 flex w-full flex-col truncate">
-                  <Tooltip text={toPascalCase(point.location)}>
-                    <div className="truncate">
-                      <span className="truncate">
-                        {toPascalCase(point.location)}
-                      </span>
-                    </div>
-                  </Tooltip>
-                  <span className="block truncate text-xs text-muted-foreground">
-                    {toPascalCase(point.district)}
-                  </span>
-                  <span className="block truncate text-xs text-muted-foreground">
-                    {toPascalCase('Sentido ' + point.direction)}
-                  </span>
-                </div>
-              </div>
+              <PointCard
+                key={index}
+                index={index}
+                location={point.location}
+                startTime={point.startTime}
+                from={point.from}
+                direction={point.direction}
+                district={point.district}
+              />
             ))
           ) : (
             <span>Nenhum resultado encontrado</span>
