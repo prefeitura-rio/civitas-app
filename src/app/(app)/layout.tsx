a8c1636f394a2c2d366b5ec9ade537171b8117cd
Victor Assis <victor_assis@poli.ufrj.br>
2024-06-28 09:17:36 -0300

    feat(Auth): :sparkles: Add Sign-out feature

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,8 @@
-import { Search, Siren } from 'lucide-react'
-import Image from 'next/image'
-import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
-import logo from '@/assets/civitas-logotype-dark.png'
 import { isAuthenticated } from '@/auth/auth'
-import { Button } from '@/components/ui/button'
-import { Tooltip } from '@/components/ui/tooltip'
+
+import { SideNavMenu } from './components/side-nav-menu'
 
 export default function AppLayout({
   children,
@@ -19,39 +15,7 @@ export default function AppLayout({
 
   return (
     <div className="flex min-h-screen">
-      <div className="flex h-screen flex-col items-center bg-card">
-        <div className="mb-8 h-12 w-12 p-2">
-          <Tooltip text="Início" side="right">
-            <Link href="/">
-              <Image
-                src={logo}
-                alt="Logo Prefeitura Rio"
-                className="h-[100%] w-auto"
-              />
-            </Link>
-          </Tooltip>
-        </div>
-        <ul className="space-y-2">
-          <li>
-            <Tooltip text="Consulta de placas" side="right">
-              <Button variant="secondary" size="sm" asChild>
-                <Link href="/consulta-de-placas">
-                  <Search className="h-4 w-4" />
-                </Link>
-              </Button>
-            </Tooltip>
-          </li>
-          <li>
-            <Tooltip text="Monitoramento de placas" side="right">
-              <Button variant="secondary" size="sm" asChild>
-                <Link href="/monitoramento-de-placas">
-                  <Siren className="h-4 w-4" />
-                </Link>
-              </Button>
-            </Tooltip>
-          </li>
-        </ul>
-      </div>
+      <SideNavMenu />
       {children}
     </div>
   )
