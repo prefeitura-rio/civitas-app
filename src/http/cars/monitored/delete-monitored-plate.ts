e0a1c4c6be80eb5db8696431a60c7be0dca3627a
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-04 07:28:05 -0300

    feat(Operações): :construction: Implementa Criação de nova operação

--- src/http/cars/monitored/delete-monitored-plate.ts
+++ src/http/cars/monitored/delete-monitored-plate.ts
@@ -1,5 +1,5 @@
 import { api } from '@/lib/api'
-import type { MonitoredPlate } from '@/models/operation'
+import type { MonitoredPlate } from '@/models/entities'
 
 export async function deleteMonitoredPlate(plate: string) {
   const response = await api.delete<MonitoredPlate>(`cars/monitored/${plate}`)
