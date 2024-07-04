5f909a949dfef3ddbffe890a0f26a62d48dd1a0d
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-04 09:18:32 -0300

    feat(Operações): :sparkles: Refatora CRUD de operações. Cria tabela, contexto, hooks, dialogs, etc

---
+++ src/app/(app)/operacoes/components/dialogs/delete-operation-alert-dialog.tsx
@@ -0,0 +1,94 @@
+import { useMutation } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
+import { useOperations } from '@/hooks/use-operations'
+import { deleteOperation } from '@/http/operations/delete-operation'
+import { queryClient } from '@/lib/react-query'
+import { genericErrorMessage } from '@/utils/error-handlers'
+
+interface DeleteOperationAlertDialogProps {
+  isOpen: boolean
+  onClose: () => void
+  onOpen: () => void
+}
+
+export function DeleteOperationAlertDialog({
+  isOpen,
+  onClose,
+  onOpen,
+}: DeleteOperationAlertDialogProps) {
+  const { onDeleteOperationProps, setOnDeleteOperationProps } = useOperations()
+
+  const { mutateAsync: deleteOperationMutation } = useMutation({
+    mutationFn: deleteOperation,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['operations'] })
+    },
+  })
+
+  async function handleDeleteOperation() {
+    try {
+      if (onDeleteOperationProps) {
+        const response = deleteOperationMutation(onDeleteOperationProps.id)
+        toast.promise(response, {
+          loading: `Excluindo operação ${onDeleteOperationProps?.title}...`,
+          success: (data) => {
+            return `Operação ${data.data.title} excluída com sucesso!`
+          },
+          error: genericErrorMessage,
+        })
+        await response
+      }
+    } catch (error) {
+      toast.error(genericErrorMessage)
+    }
+  }
+
+  function handleOnOpenChange(open: boolean) {
+    if (open) {
+      onOpen()
+    } else {
+      onClose()
+      setOnDeleteOperationProps(null)
+    }
+  }
+
+  return (
+    <AlertDialog open={isOpen} onOpenChange={handleOnOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Tem certeza que deseja excluir a operação{' '}
+            <span className="font-semibold text-destructive">
+              {onDeleteOperationProps?.title}
+            </span>
+            ?
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            Essa ação apagará definitivamente a operação{' '}
+            <span className="font-semibold text-destructive">
+              {onDeleteOperationProps?.title}
+            </span>{' '}
+            e não poderá ser desfeita.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDeleteOperation}>
+            Excluir
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
--- src/http/operations/get-operations.ts
+++ src/http/operations/get-operations.ts
@@ -2,13 +2,13 @@ import { api } from '@/lib/api'
 import type { Operation } from '@/models/entities'
 import type { PaginationRequest, PaginationResponse } from '@/models/pagination'
 
-interface getOperationsRequest extends PaginationRequest {}
+interface GetOperationsRequest extends PaginationRequest {}
 
 export interface GetOperationsResponse extends PaginationResponse {
   items: Operation[]
 }
 
-export function getOperations({ page, size }: getOperationsRequest) {
+export function getOperations({ page, size }: GetOperationsRequest) {
   return api.get<GetOperationsResponse>('/operations', {
     params: {
       page,
