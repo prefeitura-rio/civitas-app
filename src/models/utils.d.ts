ec27d6763a2fbc25ec5fa43726a2a0c0b0388455
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-04 19:18:21 -0300

    feat(Sidebar): :sparkles: Implementa  nova Sidebar

--- src/models/utils.d.ts
+++ src/models/utils.d.ts
@@ -2,3 +2,12 @@ export type ComboboxOption = {
   value: string
   label: string
 }
+
+export interface NavItem {
+  title: string
+  href: string
+  icon: LucideIcon
+  color?: string
+  isChidren?: boolean
+  children?: NavItem[]
+}
