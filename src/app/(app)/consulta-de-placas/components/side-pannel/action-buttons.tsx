f841c4d9a007609572cad4373e3aa8b5321f73ac
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-08 12:14:29 -0300

    feat(ConsultaDePlacas): :sparkles: Oculta seção "ações" quando nenhuma ação está disponível

--- src/app/(app)/consulta-de-placas/components/side-pannel/action-buttons.tsx
+++ src/app/(app)/consulta-de-placas/components/side-pannel/action-buttons.tsx
@@ -4,15 +4,23 @@ import { MonitoringToggle } from './action-buttons/monitoring-toggle'
 import DownloadReportButton from './action-buttons/report/download-report-button'
 
 export function ActionButtons() {
-  const { trips, lastSearchParams } = useCarPath()
+  const { trips, lastSearchParams, isLoading } = useCarPath()
 
   return (
-    <div className="flex flex-col">
-      <span className="text-center text-sm text-muted-foreground">Ações</span>
-      <div className="mx-6 mb-2 flex gap-2 rounded-xl border p-2">
-        {trips && trips?.length > 0 && <DownloadReportButton />}
-        {lastSearchParams && <MonitoringToggle />}
-      </div>
-    </div>
+    <>
+      {((trips && trips?.length > 0) || lastSearchParams) && !isLoading && (
+        <div className="flex flex-col">
+          <span className="text-center text-sm text-muted-foreground">
+            Ações
+          </span>
+          <div className="mx-6 mb-2 flex gap-2 rounded-xl border p-2">
+            {trips && trips?.length > 0 && !isLoading && (
+              <DownloadReportButton />
+            )}
+            {lastSearchParams && !isLoading && <MonitoringToggle />}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
