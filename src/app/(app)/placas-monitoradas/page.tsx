0df65f6159860d098c36c567523f82c538407993
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-09 14:47:21 -0300

    feat(PlacasMonitoradas): :sparkles: Implementa filtro provisório para placas monitoradas

--- src/app/(app)/placas-monitoradas/page.tsx
+++ src/app/(app)/placas-monitoradas/page.tsx
@@ -1,13 +1,14 @@
 import { Dialogs } from './components/dialogs/dialogs'
+import { MonitoredPlatesFilter } from './components/filter/monitored-plates-filter'
 import { MonitoredPlatesHeader } from './components/header/monitored-plates-header'
 import { MonitoredPlatesTable } from './components/table/monitored-plates-table'
 
 export default function PlacasMonitoradas() {
   return (
-    <div className="page-content">
+    <div className="page-content space-y-4">
       <MonitoredPlatesHeader />
+      <MonitoredPlatesFilter />
       <MonitoredPlatesTable />
-
       <Dialogs />
     </div>
   )
