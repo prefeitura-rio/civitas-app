5fb0c03e96d3ffac50bbbbb15e2a9699b93b0da3
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-12 09:00:46 -0300

    refactor: :recycle: Refactor folder structure

--- src/hooks/use-contexts/use-cameras-cor.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-
-import { CamerasCorContext } from '@/contexts/cameras-cor-context'
-
-export function useCamerasCor() {
-  const camerasCorContext = useContext(CamerasCorContext)
-
-  return camerasCorContext
-}
