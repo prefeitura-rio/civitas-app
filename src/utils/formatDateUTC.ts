bcc1c4e8877d8552e8c9bf61ce9de0e9e7a92285
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-15 14:36:04 -0300

    fix(ConsultaDePlacas): :bug: Fix de erro sistemático de parâmetro das datas de busca das consultas

--- src/utils/formatDateUTC.ts
+++ src/utils/formatDateUTC.ts
@@ -2,9 +2,9 @@ import { format } from 'date-fns'
 
 export function formatDateUTC(date: Date | string) {
   const newDate = typeof date === 'string' ? new Date(date) : date
-  const hour = newDate.getHours()
-  const dateUTC = newDate.setHours(hour + 3)
-  const formattedStringDateUTC = format(dateUTC, "yyyy-MM-dd'T'HH:mm:ss")
+  // const hour = newDate.getHours()
+  // const dateUTC = newDate.setHours(hour + 3)
+  const formattedStringDateUTC = format(newDate, "yyyy-MM-dd'T'HH:mm:ss")
 
   return formattedStringDateUTC
 }
