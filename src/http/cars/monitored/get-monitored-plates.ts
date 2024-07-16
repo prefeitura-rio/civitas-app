a9094aa5fac4e2abfaefcf73ac711d7660a80c22
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-16 12:42:09 -0300

    feat(PlacasMonitoradas): :sparkles: Implementa filtro na tabela

--- src/http/cars/monitored/get-monitored-plates.ts
+++ src/http/cars/monitored/get-monitored-plates.ts
@@ -2,7 +2,14 @@ import { api } from '@/lib/api'
 import type { BackendMonitoredPlate, MonitoredPlate } from '@/models/entities'
 import type { PaginationRequest, PaginationResponse } from '@/models/pagination'
 
-interface GetMonitoredPlatesRequest extends PaginationRequest {}
+interface GetMonitoredPlatesRequest extends PaginationRequest {
+  operationId?: string
+  operationTitle?: string
+  notificationChannelId?: string
+  notificationChannelTitle?: string
+  plateContains?: string
+  active?: boolean
+}
 
 export interface GetMonitoredPlatesResponse extends PaginationResponse {
   items: MonitoredPlate[]
@@ -13,11 +20,23 @@ interface OriginalResponse extends PaginationResponse {
 }
 
 export async function getMonitoredPlates({
+  operationId,
+  operationTitle,
+  notificationChannelId,
+  notificationChannelTitle,
+  active,
+  plateContains,
   page,
   size,
 }: GetMonitoredPlatesRequest) {
   const originalResponse = await api.get<OriginalResponse>('cars/monitored', {
     params: {
+      operation_id: operationId,
+      operation_title: operationTitle,
+      active,
+      plate_contains: plateContains,
+      notification_channel_id: notificationChannelId,
+      notification_channel_title: notificationChannelTitle,
       page,
       size,
     },
