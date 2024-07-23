5b75ce2f2b3d0f50efcbb054917e48be4ffde1c4
Victor Assis <victor_assis@poli.ufrj.br>
2024-07-23 11:31:30 -0300

    fix(Dashboard): :bug: Fix page layout

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,9 @@
 export default function Dashboard() {
   return (
-    <div className="page-content">
+    <div className="page-content pb-0 pr-0">
       <h2>Dashboard</h2>
       <iframe
-        className="h-full w-full"
+        className="h-[calc(100%-2.75rem)] w-full"
         src="https://lookerstudio.google.com/embed/reporting/28a3c678-87b1-4ca8-8462-d44d606d5a6f/page/tEnnC"
         style={{ border: 0 }}
         allowFullScreen={true}
