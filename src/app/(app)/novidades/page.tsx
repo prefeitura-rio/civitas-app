29d8999dc91eccecd4dd9909b60ddf42026d7c35
Victor Assis <victor_assis@poli.ufrj.br>
2024-08-05 19:47:28 -0300

    feat(Changelog): :sparkles: Cria componentes reutilizáveis e remove arquivos mdx e md

---
+++ src/app/(app)/novidades/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Card } from './components/card'
+import { changelog } from './components/changelog'
+
+export default function Novidades() {
+  return (
+    <div className="page-content overflow-y-scroll">
+      <div className="markdown h-full">
+        <h1>Histórico de Atualizações</h1>
+        <p>
+          Este documento registra todas as novas atualizações mais relevantes do
+          projeto CIVITAS perceptíveis ao usuário final.
+        </p>
+
+        {changelog.map((cardProps) => (
+          <Card {...cardProps} />
+        ))}
+      </div>
+    </div>
+  )
+}
