a2e96b78eecd4054db4e0245c6d7b9ee877d3878
Victor Assis <victor_assis@poli.ufrj.br>
2024-08-19 15:06:12 -0300

    feat(Ocorrências): :lipstick: Aprimora UI do hoverInfo e da timeline

--- src/app/(app)/ocorrencias/components/content/components/table/components/report-card.tsx
+++ src/app/(app)/ocorrencias/components/content/components/table/components/report-card.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Speech } from 'lucide-react'
+import { Circle, MapPin, Speech } from 'lucide-react'
 
 import { Card } from '@/components/ui/card'
 import type { Report } from '@/models/entities'
@@ -9,51 +9,64 @@ export function ReportCard(props: Report) {
   return (
     <div className="w-full pb-14">
       <Card className="-mt-10 grid grid-cols-1">
-        <div className="grid grid-cols-5 p-8">
-          <div className="col-span-1 flex gap-2">
-            <MapPin className="h-6 w-6 shrink-0" />
+        <div className="grid grid-cols-9 p-8">
+          <div className="col-span-2 flex gap-2">
+            <MapPin className="h-5 w-5 shrink-0" />
             <div className="flex flex-col">
-              <h4>Local</h4>
-              <span className="block text-muted-foreground">
-                {props.location}
+              <span className="leading-3.5 text-sm">Local</span>
+              <span className="block text-sm text-muted-foreground">
+                {props.location.capitalizeFirstLetter()}
               </span>
             </div>
           </div>
 
-          <div className="flex flex-col">
-            <h4>Tipo</h4>
-            <span className="block text-muted-foreground">
-              {props.typeAndSubtype.at(0)?.type}
-            </span>
-          </div>
-
-          <div className="flex flex-col">
-            <h4>Subtipo</h4>
-            <span className="block text-muted-foreground">
-              {props.typeAndSubtype.at(0)?.subtype.join(', ')}
-            </span>
-          </div>
-
-          <div className="flex flex-col">
-            <h4>Origem</h4>
+          <div className="col-span-1 flex flex-col">
+            <span className="leading-3.5 text-sm">Origem</span>
             <span className="block text-muted-foreground">
               {props.sourceId}
             </span>
           </div>
 
-          <div className="flex flex-col">
-            <h4>Orgãos</h4>
-            <span className="block text-muted-foreground">
-              {props.entities.map((item) => item.name).join(', ')}
-            </span>
+          <div className="col-span-3 flex flex-col">
+            <span className="leading-3.5 text-sm">Orgãos</span>
+            <ul className="text-muted-foreground">
+              {props.entities.map((item) => (
+                <li className="list-inside list-disc">
+                  {item.name.capitalizeFirstLetter()}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="col-span-3 flex flex-col">
+            <span className="leading-3.5 text-sm">Tipos e Subtipos</span>
+            <ul className="text-sm text-muted-foreground">
+              {props.typeAndSubtype.map((item) => (
+                <>
+                  <li className="list-inside list-disc">
+                    {item.type.toLocaleLowerCase()}
+                  </li>
+                  <ul>
+                    {item.subtype.map((subtype) => (
+                      <li className="list- ml-4 flex list-inside items-start gap-2">
+                        <Circle className="mt-3 size-1" />
+                        <span>{subtype.toLocaleLowerCase()}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ))}
+            </ul>
           </div>
         </div>
 
         <div className="flex gap-2 border-t-2 p-8">
           <Speech className="h-6 w-6 shrink-0" />
           <div>
-            <h4>{'Sem título'}</h4>
-            <p className="block text-muted-foreground">{props.description}</p>
+            <span className="leading-3.5 text-sm">{'Sem título'}</span>
+            <p className="block text-sm text-muted-foreground">
+              {props.description}
+            </p>
           </div>
         </div>
       </Card>
