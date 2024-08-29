3fbee18c77cd301b0e0622b370d103c914d3eee7
Victor Assis <victor_assis@poli.ufrj.br>
2024-08-29 12:31:59 -0300

    feat(Sidebar): :lipstick: Implementa nova versão do Sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,6 @@
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
-import { MonitoredPlatesContextProvider } from '@/contexts/monitored-plates-context'
-import { SidebarContextProvider } from '@/contexts/sidebar-context'
 import { CustomQueryClientProvider } from '@/hooks/query-client-provider'
 
 import { Sidebar } from './components/sidebar'
@@ -19,12 +17,8 @@ export default function AppLayout({
   return (
     <div className="flex min-h-screen">
       <CustomQueryClientProvider>
-        <SidebarContextProvider>
-          <MonitoredPlatesContextProvider>
-            <Sidebar />
-            {children}
-          </MonitoredPlatesContextProvider>
-        </SidebarContextProvider>
+        <Sidebar />
+        {children}
       </CustomQueryClientProvider>
     </div>
   )
