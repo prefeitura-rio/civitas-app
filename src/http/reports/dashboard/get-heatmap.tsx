301f9775ccd4a796ae36173043d0964d41973632
Victor Assis <victor_assis@poli.ufrj.br>
2024-09-03 08:02:15 -0300

    feat(Ocorrências): :sparkles: Implementa gráfico timeline

--- src/http/reports/dashboard/get-heatmap.tsx
+++ src/http/reports/dashboard/get-heatmap.tsx
@@ -1,17 +1,11 @@
 import { api } from '@/lib/api'
-import type { Report } from '@/models/entities'
 import type { GetReportsRequest } from '@/models/interfaces'
-import type { PaginationResponse } from '@/models/pagination'
 import { formatReportsRequest } from '@/utils/format-reports-request'
 import { toQueryParams } from '@/utils/to-query-params'
 
 export type HeatmapCoordinates = {
   latitude: number
   longitude: number
-}
-
-export interface ReportsResponse extends PaginationResponse {
-  items: Report[]
 }
 
 export async function getHeatmapReports(props: GetReportsRequest) {
