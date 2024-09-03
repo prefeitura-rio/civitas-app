f7f1dd1a11dc49056a56ddedfc22072dbd9d144f
Victor Assis <victor_assis@poli.ufrj.br>
2024-09-03 09:31:01 -0300

    feat(Ocorrências): :sparkles: Implementa gráfico de pizza de fontes

--- src/app/(app)/ocorrencias/dashboard/components/top-5-report-types-bar-chart.tsx
+++ src/app/(app)/ocorrencias/dashboard/components/top-5-report-types-bar-chart.tsx
@@ -156,14 +156,6 @@ export function Top5ReportTypesBarChart({
           </ChartContainer>
         )}
       </CardContent>
-      {/* <CardFooter className="flex-col items-start gap-2 text-sm">
-        <div className="flex gap-2 font-medium leading-none">
-          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-        </div>
-        <div className="leading-none text-muted-foreground">
-          Showing denuncias visitors for the last 6 months
-        </div>
-      </CardFooter> */}
     </Card>
   )
 }
