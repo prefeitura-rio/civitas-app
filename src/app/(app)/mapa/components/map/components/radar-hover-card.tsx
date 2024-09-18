0acbd9165b3e9a2e0085ad6fa1f0378cc18a689e
Victor Assis <victor_assis@poli.ufrj.br>
2024-09-18 07:56:05 -0300

    feat(Mapa): :sparkles: Implementa camada de câmeras do COR

--- src/app/(app)/mapa/components/map/components/radar-hover-card.tsx
+++ src/app/(app)/mapa/components/map/components/radar-hover-card.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { formatDate } from 'date-fns'
 import type { Viewport } from 'deck.gl'
+import { AlertTriangle, Building, History, MapPin } from 'lucide-react'
 import React, { type Dispatch, type SetStateAction } from 'react'
 
-import { TooltipInfoItem } from '@/app/(app)/mapa-[old]/components/common/tooltip-info-item'
 import { MapHoverCard } from '@/components/custom/map-hover-card'
+import { Separator } from '@/components/ui/separator'
+import { cn } from '@/lib/utils'
 import type { Radar } from '@/models/entities'
 
 interface HoverCardProps {
@@ -21,6 +23,29 @@ export function RadarHoverCard({
   y,
   setIsHoveringInfoCard,
 }: HoverCardProps) {
+  const Label = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode
+    className?: string
+  }) => (
+    <span className={cn('text-sm font-semibold', className)}>{children}</span>
+  )
+
+  const Value = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode
+    className?: string
+  }) => (
+    <span
+      className={cn('text-sm font-semibold text-muted-foreground', className)}
+    >
+      {children}
+    </span>
+  )
   return (
     <MapHoverCard x={x} y={y} object={radar} viewport={viewport}>
       {radar && (
@@ -32,41 +57,100 @@ export function RadarHoverCard({
             setIsHoveringInfoCard(false)
           }}
         >
-          <TooltipInfoItem label="Número Câmera" value={radar.cameraNumber} />
-          <TooltipInfoItem
-            label="Código CET-Rio"
-            value={radar.cetRioCode || ''}
-          />
-          <TooltipInfoItem label="Localização" value={radar.location || ''} />
-          <TooltipInfoItem label="Latitude" value={radar.latitude.toString()} />
-          <TooltipInfoItem
-            label="Longitude"
-            value={radar.longitude.toString()}
-          />
-          <TooltipInfoItem label="Bairro" value={radar.district || ''} />
-          <TooltipInfoItem label="Logradouro" value={radar.streetName || ''} />
-          <TooltipInfoItem label="Sentido" value={radar.direction || ''} />
-          <TooltipInfoItem label="Empresa" value={radar.company || ''} />
-          <TooltipInfoItem
-            label="Última detecção"
-            value={
-              radar.lastDetectionTime
-                ? formatDate(
-                    radar.lastDetectionTime,
-                    "dd/MM/yyyy 'às' HH:mm:ss",
-                  )
-                : ''
-            }
-          />
-          <TooltipInfoItem
-            label="Ativo nas últimas 24 horas"
-            value={radar.activeInLast24Hours ? 'Sim' : 'Não'}
-          />
-          <p className="text-sm text-muted-foreground">
-            ⚠️ Atenção! Radares são considerados inativos se não enviarem dados
-            há mais de 24 horas. No entanto, essa informação não é atualizada em
-            tempo real e pode seguir desatualizada por várias horas.
-          </p>
+          <h4>Informações do Radar</h4>
+          <Separator className="mb-4 mt-1 bg-secondary" />
+          <div className="flex flex-col gap-2">
+            <div className="grid grid-cols-2">
+              <div className="flex flex-col">
+                <Label>Número Câmera</Label>
+                <Value>{radar.cameraNumber}</Value>
+              </div>
+
+              <div className="flex flex-col">
+                <Label>Código CET-Rio</Label>
+                <Value>{radar.cetRioCode}</Value>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex items-center gap-1">
+                <MapPin className="shinrk-0 size-3.5" />
+                <Label>Localização</Label>
+              </div>
+              <Value>{`${radar.location} - ${radar.district}`}</Value>
+            </div>
+
+            <div className="grid grid-cols-2 gap-2">
+              <div className="flex flex-col">
+                <Label>Latitude</Label>
+                <Value>{radar.latitude}</Value>
+              </div>
+              <div className="flex flex-col">
+                <Label>Longitude</Label>
+                <Value>{radar.longitude}</Value>
+              </div>
+              {/* {radar.district && (
+                <div className="">
+                  <Label>Bairro: </Label>
+                  <Value>{radar.district}</Value>
+                </div>
+              )} */}
+            </div>
+
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col">
+                <div className="flex items-center gap-1">
+                  <Building className="size-4 shrink-0" />
+                  <Label>Empresa</Label>
+                </div>
+                <Value>{radar.company}</Value>
+              </div>
+              {radar.lastDetectionTime && (
+                <div className="flex flex-col gap-2">
+                  <div className="flex flex-col">
+                    <div className="flex items-center gap-1">
+                      {/* <Calendar className="size-4 shrink-0" /> */}
+                      <History
+                        className={cn(
+                          'size-4 shrink-0',
+                          // radar.activeInLast24Hours
+                          //   ? 'text-emerald-600'
+                          //   : 'text-rose-600',
+                        )}
+                      />
+                      <Label>Última detecção</Label>
+                    </div>
+                    <Value>
+                      {formatDate(
+                        radar.lastDetectionTime,
+                        "dd/MM/y 'às' HH:mm:ss",
+                      )}
+                    </Value>
+                  </div>
+                  <div className="flex flex-col">
+                    <Label>Ativo nas últimas 24 horas</Label>
+                    {radar.activeInLast24Hours ? (
+                      <Value className="text-emerald-600">Sim</Value>
+                    ) : (
+                      <Value className="text-rose-600">Não</Value>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="border-l-4 border-yellow-600 bg-secondary p-2">
+              <div className="flex items-start">
+                <AlertTriangle className="mr-2 mt-1 h-6 w-6 text-yellow-400" />
+                <p className="text-sm text-gray-300">
+                  <span className="font-bold text-yellow-400">Atenção!</span>{' '}
+                  Radares são considerados inativos se não enviarem dados há
+                  mais de 24 horas. No entanto, essa informação não é atualizada
+                  em tempo real e pode seguir desatualizada por várias horas.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </MapHoverCard>
---
+++ src/hooks/map-layers/use-cameras.tsx
@@ -0,0 +1,85 @@
+import type { PickingInfo } from '@deck.gl/core'
+import { IconLayer } from '@deck.gl/layers'
+import { useQuery } from '@tanstack/react-query'
+import { type Dispatch, type SetStateAction, useState } from 'react'
+
+import cameraIconAtlas from '@/assets/camera-icon-atlas.png'
+import { getCameraCOR } from '@/http/cameras-cor/get-cameras-cor'
+import { CameraCOR } from '@/models/entities'
+
+export interface UseCameraCOR {
+  data: CameraCOR[]
+  failed: boolean
+  layer: IconLayer<CameraCOR, object>
+  isLoading: boolean
+  isVisible: boolean
+  setIsVisible: Dispatch<SetStateAction<boolean>>
+  hoveredObject: PickingInfo<CameraCOR> | null
+  setHoveredObject: Dispatch<SetStateAction<PickingInfo<CameraCOR> | null>>
+  selectedObject: CameraCOR | null
+  setSelectedObject: Dispatch<SetStateAction<CameraCOR | null>>
+}
+
+export function useCameraCOR(): UseCameraCOR {
+  const [hoveredObject, setHoveredObject] =
+    useState<PickingInfo<CameraCOR> | null>(null)
+  const [isVisible, setIsVisible] = useState(false)
+  const [selectedObject, setSelectedObject] = useState<CameraCOR | null>(null)
+
+  const { data, isLoading } = useQuery({
+    queryKey: ['cameras'],
+    queryFn: () => getCameraCOR(),
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  })
+
+  const layer = new IconLayer<CameraCOR>({
+    id: 'cameras',
+    data,
+    pickable: true,
+    getSize: 24,
+    autoHighlight: true,
+    highlightColor: [7, 76, 128, 250], // CIVITAS-dark-blue
+    visible: isVisible,
+    iconAtlas: cameraIconAtlas.src,
+    iconMapping: {
+      default: {
+        x: 0,
+        y: 0,
+        width: 48,
+        height: 48,
+        mask: false,
+      },
+      highlighted: {
+        x: 48,
+        y: 0,
+        width: 48,
+        height: 48,
+        mask: false,
+      },
+    },
+    getIcon: (d) => {
+      if (selectedObject?.code === d.code) {
+        return 'highlighted'
+      } else return 'default'
+    },
+    getPosition: (info) => [info.longitude, info.latitude],
+    onHover: (info) => setHoveredObject(info),
+    onClick: (info) => {
+      setSelectedObject(info.object)
+    },
+  })
+
+  return {
+    data: data || [],
+    failed: !data && !isLoading,
+    layer,
+    isLoading,
+    isVisible,
+    setIsVisible,
+    hoveredObject,
+    setHoveredObject,
+    selectedObject,
+    setSelectedObject,
+  }
+}
