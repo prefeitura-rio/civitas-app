3b38ae8f98ab82b190e355f80145e96c39018f43
Victor Assis <victor_assis@poli.ufrj.br>
2024-09-18 11:47:22 -0300

    fix(Mapa): :bug: Corrige bug de carregamento infinito das imagens das câmeras do COR

--- src/app/(app)/mapa/components/map/components/hover-cards/camera-hover-card.tsx
+++ src/app/(app)/mapa/components/map/components/hover-cards/camera-hover-card.tsx
@@ -41,7 +41,7 @@ export function CameraHoverCard({
       >
         <h4>Informações da Câmera</h4>
         <Separator className="mb-4 mt-1 bg-secondary" />
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-4">
           <div className="flex flex-col">
             <Label>Código</Label>
             <Value>{object?.code}</Value>
