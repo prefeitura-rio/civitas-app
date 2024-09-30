c8ebaa95f3a472cfbee4be5cd2142090b632b859
Victor Assis <victor_assis@poli.ufrj.br>
2024-09-30 13:50:19 -0300

    feat(Sidebar): :lipstick: Altera visualização da sidebar

--- src/app/(app)/components/sidebar/components/constants.ts
+++ src/app/(app)/components/sidebar/components/constants.ts
@@ -1,49 +1,50 @@
 import type { icons } from 'lucide-react'
 
-export interface ModuleProduct {
+export interface Module {
   icon: keyof typeof icons
   title: string
   path: string
 }
 
-export interface Module {
+export interface Category {
   icon: keyof typeof icons
   title: string
-  products: ModuleProduct[]
+  modules: Module[]
 }
 
-export const home: ModuleProduct = {
-  icon: 'Home',
-  title: 'Início',
-  path: '/',
-}
+export type SideBarItem = Module | Category
 
-export const modules: Module[] = [
+export const sidebarItems: SideBarItem[] = [
   {
-    icon: 'Package',
-    title: 'Módulos',
-    products: [
+    icon: 'Car',
+    title: 'Veículos',
+    path: '/veiculos',
+  },
+  {
+    icon: 'Users',
+    title: 'Pessoas',
+    path: '/pessoas',
+  },
+  {
+    icon: 'Speech',
+    title: 'Ocorrências (DD & 1746)',
+    path: '/ocorrencias/timeline',
+  },
+  {
+    icon: 'LayoutDashboard',
+    title: 'Dashboards',
+    modules: [
       {
-        icon: 'Car',
-        title: 'Veículos',
-        path: '/veiculos',
-      },
-      {
-        icon: 'Users',
-        title: 'Pessoas',
-        path: '/pessoas',
-      },
-      {
-        icon: 'Speech',
-        title: 'Ocorrências (DD & 1746)',
-        path: '/ocorrencias/timeline',
+        icon: 'Cctv',
+        title: 'Status Infra dos Radares',
+        path: 'dashboards/status-infra-radares',
       },
     ],
   },
   {
     icon: 'Settings',
     title: 'Configurações',
-    products: [
+    modules: [
       {
         icon: 'Rss',
         title: 'Placas Monitoradas',
@@ -53,17 +54,6 @@ export const modules: Module[] = [
         icon: 'TrafficCone',
         title: 'Cadastro de Operações',
         path: '/operacoes',
-      },
-    ],
-  },
-  {
-    icon: 'LayoutDashboard',
-    title: 'Dashboards',
-    products: [
-      {
-        icon: 'Cctv',
-        title: 'Status Infra dos Radares',
-        path: 'dashboards/status-infra-radares',
       },
     ],
   },
