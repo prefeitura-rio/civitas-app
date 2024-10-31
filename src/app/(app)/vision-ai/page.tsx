ea9cd204c844b0b0952228b668f0b8684a0c464c
Victor Assis <victor_assis@poli.ufrj.br>
2024-10-31 15:35:59 -0300

    feat: :sparkles: Implementa comunicação com app Vision AI

--- src/app/(app)/vision-ai/page.tsx
+++ src/app/(app)/vision-ai/page.tsx
@@ -1,7 +1,12 @@
-export default function Page() {
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = cookies()
+  const token = cookieStore.get('token')?.value
+
   return (
     <iframe
-      src="https://super-app-blond-eight.vercel.app/vision-ai"
+      src={`http://localhost:3000/vision-ai?token=${token}`}
       className="h-screen w-full"
       title="Vision AI"
     />
