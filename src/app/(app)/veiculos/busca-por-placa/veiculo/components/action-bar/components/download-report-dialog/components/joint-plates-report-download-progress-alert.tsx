34dbf51af4f2fd5c26c1e24d669cb7f25c440339
Victor Assis <victor_assis@poli.ufrj.br>
2024-12-13 10:22:12 -0300

    feat: :sparkles: Implementa opção de download de relatórios de busca por placa em CSV

--- src/app/(app)/veiculos/busca-por-placa/veiculo/components/action-bar/components/download-report-dialog/components/joint-plates-report-download-progress-alert.tsx
+++ src/app/(app)/veiculos/busca-por-placa/veiculo/components/action-bar/components/download-report-dialog/components/joint-plates-report-download-progress-alert.tsx
@@ -1,20 +1,28 @@
 import { pdf } from '@react-pdf/renderer'
 import { useQuery } from '@tanstack/react-query'
+import { formatDate } from 'date-fns'
 import React, { useEffect, useState } from 'react'
 
 import { Spinner } from '@/components/custom/spinner'
 import { AlertDialog, AlertDialogContent } from '@/components/ui/alert-dialog'
 import { useCarPathsSearchParams } from '@/hooks/use-params/use-car-paths-search-params'
 import { useNCarsBeforeAfter } from '@/hooks/use-queries/cars/use-n-before-after'
+import { exportToCSV } from '@/utils/csv'
 import { downloadFile } from '@/utils/download-file'
 
 import { ReportDocument } from '../../reports-by-detection-point/components/report/document'
+
+enum FileType {
+  'PDF' = 'PDF',
+  'CSV' = 'CSV',
+}
 
 interface DownloadReportsByDetectionPointButtonProps {
   open: boolean
   setOpen: (open: boolean) => void
   nMinutes: number
   nPlates: number
+  fileType: FileType
 }
 
 export default function JointPlatesReportDownloadProgressAlert({
@@ -22,6 +30,7 @@ export default function JointPlatesReportDownloadProgressAlert({
   setOpen,
   nMinutes,
   nPlates,
+  fileType,
 }: DownloadReportsByDetectionPointButtonProps) {
   const { formattedSearchParams } = useCarPathsSearchParams()
   const [initialized, setInitialized] = useState(false)
@@ -63,7 +72,7 @@ export default function JointPlatesReportDownloadProgressAlert({
       )
       return doc.toBlob()
     },
-    enabled: initialized2,
+    enabled: initialized2 && fileType === FileType.PDF,
   })
 
   useEffect(() => {
@@ -73,8 +82,64 @@ export default function JointPlatesReportDownloadProgressAlert({
     if (open && data && !isPending) {
       setInitialized2(true)
     }
-    if (open && data && !isPending && doc && !isGeneratingDocument) {
+    if (
+      open &&
+      data &&
+      !isPending &&
+      fileType === FileType.PDF &&
+      doc &&
+      !isGeneratingDocument
+    ) {
       downloadFile(doc, `placas_conjuntas_${formattedSearchParams.plate}.pdf`)
+      setOpen(false)
+    }
+    if (open && data && !isPending && fileType === FileType.CSV) {
+      const diff =
+        new Date(formattedSearchParams.to).getTime() -
+        new Date(formattedSearchParams.from).getTime()
+
+      exportToCSV(
+        `placas_conjuntas_${formattedSearchParams.plate}`,
+        data.groups.flatMap((g, i) =>
+          g.detections.map((d, j) => ({
+            'Índice Grupo': i + 1,
+            'Índice Detecção': j + 1,
+            'Data Hora Detecção Placa Monitorada': formatDate(
+              new Date(
+                new Date(formattedSearchParams.from).getTime() + diff / 2,
+              ),
+              'dd/MM/yyyy HH:mm:ss',
+            ),
+            'Início Período Analisado': formatDate(
+              g.start_time,
+              'dd/MM/yyyy HH:mm:ss',
+            ),
+            'Fim Período Analisado': formatDate(
+              g.end_time,
+              'dd/MM/yyyy HH:mm:ss',
+            ),
+            Radares: g.radars.join(', '),
+            Latitude: g.latitude.toString().replace('.', ','),
+            Longitude: g.longitude.toString().replace('.', ','),
+            Endereço: g.location,
+            'Total Detecções Grupo': g.total_detections,
+            'Data e Hora Detecção': d.timestamp,
+            Placa: d.plate,
+            Radar: d.camera_numero,
+            Faixa: d.lane,
+            'Velocidade (km/h)': d.speed,
+            'Nº Ocorrências': d.count,
+          })),
+        ),
+      )
+      exportToCSV(
+        `placas_conjuntas_${formattedSearchParams.plate}_ranking`,
+        data.ranking.map((r) => ({
+          Placa: r.plate,
+          Contagem: r.count,
+        })),
+      )
+
       setOpen(false)
     }
   }, [isPending, data, open, isGeneratingDocument, doc])
