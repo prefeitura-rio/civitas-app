e2115b9339709233debea52002857ccc514b0af2
Victor Assis <victor_assis@poli.ufrj.br>
2025-01-13 18:20:29 -0300

    fix: :bug: Corrige unidade de medida dos sliders

--- src/components/custom/double-slider.tsx
+++ src/components/custom/double-slider.tsx
@@ -7,8 +7,10 @@ import { cn } from '@/lib/utils'
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
+    unity?: string
+  }
+>(({ className, unity, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -23,13 +25,13 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Thumb className="relative block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
       {/* tip: absolute left distance = half of thumb width (2.5) - half of caption width (6) = -3.5 */}
       <span className="absolute -top-6 right-0 w-12 whitespace-nowrap text-center text-sm">
-        {!!props.value?.at(0) && `${props.value?.at(0)} min`}
+        {!!props.value?.at(0) && `${props.value?.at(0)} ${unity}`}
       </span>
     </SliderPrimitive.Thumb>
     <SliderPrimitive.Thumb className="relative block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
       {/* tip: absolute left distance = half of thumb width (2.5) - half of caption width (6) = -3.5 */}
       <span className="absolute -top-6 left-0 w-12 whitespace-nowrap text-center text-sm">
-        {!!props.value?.at(1) && `${props.value?.at(1)} min`}
+        {!!props.value?.at(1) && `${props.value?.at(1)} ${unity}`}
       </span>
     </SliderPrimitive.Thumb>
   </SliderPrimitive.Root>
