e7d3590ef3d4dc59f367330a2fb8858f7da7a599
nicolasevilasio <nicolas_evilasio@hotmail.com>
2025-08-15 17:06:47 -0300

    refactor(dashboard): update Looker Studio embed URL for status infra radares

--- src/app/(app)/dashboards/status-infra-radares/page.tsx
+++ src/app/(app)/dashboards/status-infra-radares/page.tsx
@@ -3,7 +3,7 @@ export default function StatusInfraRadares() {
     <div className="page-content p-0">
       <iframe
         className="h-full w-full"
-        src="https://lookerstudio.google.com/embed/reporting/28a3c678-87b1-4ca8-8462-d44d606d5a6f/page/tEnnC"
+        src="https://lookerstudio.google.com/embed/reporting/b62fd5d4-6975-4320-adf3-eb089d6e03e5/page/tEnnC"
         style={{ border: 0 }}
         allowFullScreen={true}
         sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
