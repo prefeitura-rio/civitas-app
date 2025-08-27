f3332d7b5136b03ac0bd8cefc0d14a22383545bc
Gabriel Seixas <salesseixasgabriel@gmail.com>
2025-08-27 17:41:25 -0300

    feat: configurar Jest e testes básicos funcionando

---
+++ jest.setup.js
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom'
+
+// Suppress console.error for cleaner test output
+const originalError = console.error
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      typeof args[0] === 'string' &&
+      args[0].includes('Warning: ReactDOM.render is no longer supported')
+    ) {
+      return
+    }
+    originalError.call(console, ...args)
+  }
+})
+
+afterAll(() => {
+  console.error = originalError
+})
