4560a389840402ca2d3bacd6fa42bf262270e75c
Gabriel Seixas <salesseixasgabriel@gmail.com>
2025-12-02 11:14:27 -0300

    Revert "Feat/gcs signed url refactor"

--- src/app/(app)/gcs/download-url/page.tsx
+++
@@ -1,9 +0,0 @@
-import { GcsDownloadUrlForm } from './components/gcs-download-url-form'
-
-export default function GcsDownloadUrlPage() {
-  return (
-    <div className="page-content flex items-center justify-center overflow-y-scroll p-6">
-      <GcsDownloadUrlForm />
-    </div>
-  )
-}
--- src/http/gcs/constants.ts
+++
@@ -1,7 +0,0 @@
-export const GCS_ROUTES = {
-  files: {
-    'download-url': '/gcs/files/download-url',
-    'upload-url': '/gcs/upload-url',
-    upload: '/gcs/upload',
-  },
-} as const
