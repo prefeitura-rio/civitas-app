1a66f1959ef8f729740d9b1c06c7661727e85bc1
martinsmirai <matheus.martins@studiomirai.com>
2026-04-13 09:27:12 -0300

    refactor: clean up code formatting in various components for consistency

--- src/components/custom/double-slider.tsx
+++ src/components/custom/double-slider.tsx
@@ -24,7 +24,7 @@ const Slider = React.forwardRef<
       ref={ref}
       className={cn(
         'relative flex w-full touch-none select-none items-center',
-        className,
+        className
       )}
       {...props}
     >
