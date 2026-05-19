f6ed96aecd88fb46a41e955b30923e4ff7b0f53a
nicolasevilasio <nicolas_evilasio@hotmail.com>
2026-05-19 11:49:42 -0300

    fix(changelog): prevent orphan list marker on long entry titles

--- src/app/(app)/novidades/components/card/components/sub-card/index.tsx
+++ src/app/(app)/novidades/components/card/components/sub-card/index.tsx
@@ -3,15 +3,11 @@ import { Tag } from './components/tag'
 
 export function SubCard({ title, tag, content }: SubCard) {
   return (
-    <>
-      <li>
-        <h3 className="inline-block">
-          <Tag tag={tag} /> {title}
-        </h3>
-      </li>
-      <ul>
-        <li className="list-none">{content}</li>
-      </ul>
-    </>
+    <li>
+      <h3 className="!mt-0 mb-4 text-xl">
+        <Tag tag={tag} /> {title}
+      </h3>
+      <div>{content}</div>
+    </li>
   )
 }
